1c0ea0deb5c7dbca64ddf0ad738b21fa93f6d1a7
Maycon <mayconbeserra@hotmail.com>
2017-02-08 20:09:42 +0100

    cleanup

--- .bin/service.js
+++
@@ -1,5 +0,0 @@
-// enables ES6 support
-require('babel-core/register');
-require('babel-polyfill');
-
-require(process.env.NODE_ENV === 'production' ? '../lib' : '../src/app.js').start(require('../config').default);
